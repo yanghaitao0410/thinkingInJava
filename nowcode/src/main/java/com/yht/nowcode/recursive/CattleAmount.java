05fdb5c8561ce19c8d2119334f20071facde8ee3
Unknown <1454385822@qq.com>
2018-11-07 10:04:31 +0800

    递归（最小路径和、母牛问题）

---
+++ nowcode/src/main/java/com/yht/nowcode/recursive/CattleAmount.java
@@ -0,0 +1,70 @@
+package com.yht.nowcode.recursive;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 母牛每年生一只母牛，新出生的母牛成长3年后也能每年生一只母牛，假设不会死。
+ * 求N年后，母牛的数量。
+ * 分析过程：
+ * 年份 1    2     3     4    5    6     7
+ *     A    A     A     A    A    A     A
+ *          B     B     B    B    B     B
+ *                C     C    C    C     C
+ *                      D    D    D     D
+ *                           E    E     E
+ *                           F    F     F
+ *                                G     G
+ *                                H     H
+ *                                I     I
+ *                                      J
+ *                                      K
+ *                                      L
+ *                                      M
+ *数量 1     2    3     4    6    9
+ * 分析得出：
+ *    第n年的牛等于 n-1年全部的牛 + 以成熟的母牛生的新母牛
+ *    第5年开始：
+ *    F(N) = F(N-1) + F(N-3)
+ *
+ *
+ * 进阶：
+ *  假设每只母牛只能活10年，求N年后，母牛的数量
+ *  10年前和之前一样
+ *  第11年母牛数量=第10年全部数量+第8年全部数量- 第1年母牛数量(死亡) - 第1年母牛数量(没有出生)
+ *  todo 感觉不对
+ */
+public class CattleAmount {
+    Map<Integer, Long> amountYearMap;
+    public CattleAmount(){
+        amountYearMap = new HashMap<>();
+    }
+
+    public long getCattleAmount(int N) {
+        if(N < 0) {
+            return 0;
+        }
+        if(N <= 4) {
+            return N;
+        }
+        return getAmount(N - 1) + getAmount(N - 3);
+    }
+
+    private long getAmount(int year) {
+        Long amount = amountYearMap.get(year);
+        if(amount == null) {
+            amount = getCattleAmount(year);
+            amountYearMap.put(year, amount);
+        }
+        return amount;
+    }
+
+    @Test
+    public void test() {
+        Long start = System.currentTimeMillis();
+        System.out.printf("Cattle Amount: %d", getCattleAmount(6));
+        System.out.printf("计算花费%s毫秒", System.currentTimeMillis() - start);
+    }
+}
---
+++ nowcode/src/main/java/com/yht/nowcode/recursive/MinSum.java
@@ -0,0 +1,62 @@
+package com.yht.nowcode.recursive;
+
+import org.junit.Test;
+
+/**
+ * 给你一个二维数组，二维数组中每个数都是正数，要求从左上角走到右下角，每一步只能向右或向下。
+ * 沿途的数字要累加起来。返回最小的路径和。
+ */
+public class MinSum {
+
+    public static int getMinSum(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] minSumArr = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                minSumArr[i][j] = -1;
+            }
+        }
+        return process(matrix, minSumArr,0, 0);
+    }
+
+    /**
+     * 暴力递归版
+     * 当前在matrix[i][j] 位置上
+     * 假如走到最下行，接下来只能向右走
+     * 假如走到最右列，接下开只能向下走
+     * 其他情况，当前位置值 + 剩下位置最小路径和
+     *
+     * @param matrix
+     * @param i
+     * @param j
+     * @return
+     */
+    public static int process(int[][] matrix, int[][] minSumArr, int i, int j) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int curValue = matrix[i][j];
+        if (i == row - 1 && j == col - 1) { //走到最后，返回最后位置的值
+            return curValue;
+        }
+        if (i == row - 1) { //走到了最下行，只能向右走
+            return curValue + getValue(matrix, minSumArr, i, j + 1);
+        }
+        if (j == col - 1) {
+            return curValue + getValue(matrix, minSumArr,i + 1, j);
+        }
+        return curValue + Math.min(getValue(matrix, minSumArr,i + 1, j), process(matrix, minSumArr, i, j + 1));
+    }
+
+    private static int getValue(int[][] matrix, int[][] minSumArr, int i, int j) {
+        if (minSumArr[i][j] == -1) {
+            minSumArr[i][j] = process(matrix, minSumArr, i, j);
+        }
+        return minSumArr[i][j];
+    }
+
+    @Test
+    public void test() {
+        System.out.println(getMinSum(new int[][]{{3, 2, 1, 0}, {7, 5, 0, 1}, {3, 7, 6, 2}}));
+    }
+}
