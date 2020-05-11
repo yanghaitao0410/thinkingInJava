b959cfc1f1c6fbf0a6a91f1f2b43204b7b6fd8ce
[yanghaitao0410] <1454385822@qq.com>
2020-05-11 23:10:10 +0800

    getMinStack 注释修改

--- nowcode/src/main/java/com/yht/nowcode/basicstructure/GetMinStack2.java
+++ nowcode/src/main/java/com/yht/nowcode/basicstructure/GetMinStack2.java
@@ -4,8 +4,9 @@ import java.util.Stack;
 
 /**
  * 优化版本：
- *      压入操作：当前数和min栈顶元素比较，小于等于才压入     大于不压入
- *      弹出操作：data正常弹，和min栈顶元素比较，相等min栈才弹，否则不弹
+ *      压入操作：当前数和min栈顶元素比较，小于等于才压入（等于时需要压入 否则弹出时不知道data剩下的元素是否还有min中的栈顶元素）
+ *          大于不压入
+ *      弹出操作：data中数据正常弹出，min中数据：data弹出数据和min栈顶元素比较，相等min栈弹出，否则不弹出
  */
 public class GetMinStack2 {
     private Stack<Integer> data;
